8b1b545af176e0ca8b0a547b62dcf59d73958388
Daron <daronspence@gmail.com>
2018-05-22 00:37:57 -0500

    github pages compat

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const Root = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
 
